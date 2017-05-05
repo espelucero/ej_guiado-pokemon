9a7ccf7ca769b90e1ba32ee964e7a9d78931f94e
“espelucero” <espe.lucero@gmail.com>
2017-05-04 23:32:53 -0300

    Ejercicio 1: Continuación Pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 function Pokemon(nombre,color, poderDeAtaque){
 	this.nombre = nombre;
 	this.color = color;
@@ -20,9 +21,31 @@ function Pokemon(nombre,color, poderDeAtaque){
 	};
 }
 
-const Pikachu = new Pokemon('Pikachu', 'amarillo', 100)
+/*const Pikachu = new Pokemon('Pikachu', 'amarillo', 100)
 const Charmander = new Pokemon('Charmander', 'rojo' , 20)
+const Balbasaur = new Pokemon('Balbasaur','verde', 60)
+const Squirtle = new Pokemon('Squirtle', 'azul', 70)
+*/
 
-Pikachu.atacar(Charmander)
+function botonPelear(){
+	var ataque1 = document.getElementById('pokemon1').value;
+	var ataque2 = document.getElementById('pokemon2').value;
 
-console.log(Charmander.vida)
+	var opcion1 = new Pokemon(ataque1, 170);
+	var opcion2 = new Pokemon(ataque2, 60);
+
+	var parrafo = document.getElementById('pelear');
+
+	if(ataque1 == ataque2){
+		return alert("NO puedes atacar al mismo Pokemon ");
+	}
+
+	var resultAtaque = ('<br>' + ataque1 + 
+	' atacó a ' + ataque2 + ' y ' + ataque2 + ' tiene ' + opcion2.vida + ' de vida restante' );
+	parrafo.innerHTML = resultAtaque
+}
+
+
+//Pikachu.atacar(Charmander)
+
+//console.log(Charmander.vida)
